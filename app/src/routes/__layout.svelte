a6cd321bf37732687f73156a294b0471c37df0be
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-07-18 18:26:39 -0400

    feat: frontend base page

--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -1,15 +1,28 @@
-<div>
-  <slot />
-</div>
+<script lang="ts">
+  import { apiUrl } from '@app/env'
+  const loginToTwitchUrl = `${apiUrl}/login`
+</script>
+
+<nav class="navbar is-light" aria-label="main navigation">
+  <div class="navbar-brand" />
+  <div class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="/">Home</a>
+    </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <a class="button is-primary" href={loginToTwitchUrl}>
+          Log in with Twitch
+        </a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<slot />
 
 <style lang="scss">
   :global {
-    @import 'reset.scss';
     @import 'global.scss';
-  }
-
-  div {
-    height: 100vh;
-    width: 100vw;
   }
 </style>
