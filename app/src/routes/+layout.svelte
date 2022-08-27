109f6027cf644112a69641ce8737acdd9fb238c9
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-08-27 00:32:01 -0400

    feat: better navbar

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -5,17 +5,55 @@
   // Props
   export let data: LayoutData
 
+  // Binding
+  let menu: HTMLDivElement
+
+  // State
+  let menuIsOpen = false
+
   // Variables
   const { user, isBot } = data
   const logged = user != null
   const loggedUserName = logged ? `${isBot ? '[BOT]' : ''} ${user.username}` : undefined
+
+  function closeMenuOnNavigate(event: Event) {
+    menuIsOpen = false
+    event.target?.removeEventListener('click', closeMenuOnNavigate)
+  }
+
+  function toggleMenu() {
+    if (!menuIsOpen) {
+      menu.querySelectorAll('a').forEach(el => {
+        el.addEventListener('click', closeMenuOnNavigate)
+      })
+    }
+    menuIsOpen = !menuIsOpen
+  }
 </script>
 
 <nav class="navbar is-light" aria-label="main navigation">
-  <div class="navbar-brand" />
-  <div class="navbar-menu">
+  <div class="navbar-brand">
+    <button
+      type="button"
+      class="navbar-burger"
+      class:is-active={menuIsOpen}
+      on:click={toggleMenu}
+    >
+      <span />
+      <span />
+      <span />
+    </button>
+  </div>
+  <div bind:this={menu} class="navbar-menu" class:is-active={menuIsOpen}>
     <div class="navbar-start">
       <a class="navbar-item" href="/">Home</a>
+      {#if user && isBot}
+        <a class="navbar-item" href="/bot">Bot settings</a>
+      {/if}
+
+      {#if user && !isBot}
+        <a class="navbar-item" href="/app">Dashboard</a>
+      {/if}
     </div>
     <div class="navbar-end">
       {#if loggedUserName}
