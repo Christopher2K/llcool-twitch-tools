18390660eb2bf67b0a79063f4fb1da971368e75d
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-08-28 18:53:05 -0400

    fix: layout for app side

--- app/src/routes/app/+layout.svelte
+++ app/src/routes/app/+layout.svelte
@@ -1,1 +1,15 @@
-<slot />
+<script lang="ts">
+  import SideMenuItem from '@app/components/SideMenuItem.svelte'
+  import DashboardLayout from '@app/components/DashboardLayout.svelte'
+</script>
+
+<DashboardLayout title="Dashboard">
+  <svelte:fragment slot="links">
+    <SideMenuItem href="/app" label="Dashboard" />
+    <SideMenuItem href="/app/commands" label="Commands" />
+  </svelte:fragment>
+
+  <svelte:fragment slot="content">
+    <slot />
+  </svelte:fragment>
+</DashboardLayout>
---
+++ app/src/routes/app/commands/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Typography from '@app/components/Typography.svelte'
+</script>
+
+<Typography tag="h1">Commands</Typography>
+<Typography>TDB</Typography>
+
