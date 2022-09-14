65cdbe4ceefa7df8000830b37e3db2b71467c604
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-09-14 09:39:44 -0400

    fix: remove unused

--- app/src/routes/app/+page.ts
+++ app/src/routes/app/+page.ts
@@ -2,8 +2,6 @@ import { getBotInfo } from '@app/api'
 
 import type { PageLoad } from './$types'
 
-export const prerender = true
-
 export const load: PageLoad = async ({ fetch }) => {
   const botInfo = await getBotInfo(fetch)
 
