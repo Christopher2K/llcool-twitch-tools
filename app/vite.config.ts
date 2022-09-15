4356ddcd080d23f1f083a07b13cc2b4aaaf3b180
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-09-15 19:46:51 -0400

    chore: fix fe build prod config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,14 +1,24 @@
 import { readFileSync } from 'fs'
-import { defineConfig } from 'vite'
-import { sveltekit } from '@sveltejs/kit/vite' 
+import { defineConfig, type PluginOption, type UserConfig } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite'
 import svg from '@poppanator/sveltekit-svg'
 
-export default defineConfig({
-  server: {
-    https: {
-      cert: readFileSync(`${__dirname}/../localhost.pem`),
-      key: readFileSync(`${__dirname}/../localhost-key.pem`),
-    },
-  },
-  plugins: [sveltekit(), svg({})],
+export default defineConfig(({ command }) => {
+  const commonConfig: UserConfig = {
+    plugins: [sveltekit(), svg({}) as PluginOption],
+  }
+
+  if (command === 'build') {
+    return commonConfig
+  } else {
+    return {
+      ...commonConfig,
+      server: {
+        https: {
+          cert: readFileSync(`${__dirname}/../localhost.pem`),
+          key: readFileSync(`${__dirname}/../localhost-key.pem`),
+        },
+      },
+    }
+  }
 })
