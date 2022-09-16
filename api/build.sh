a2564b7f6c484205958549b363bc435c9d98c16b
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-09-15 20:44:59 -0400

    chore: use a run script for backend

--- api/build.sh
+++ api/build.sh
@@ -3,14 +3,6 @@
 # CLI installation
 cargo install diesel_cli
 
-# DB migrations
-if [[ $DATABASE_URL == "" ]]; then
-	echo "DATABASE_URL is not defined"
-	exit 1
-fi
-
-diesel migration run
-
 # Build
 cargo build --release
 
