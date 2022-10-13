ace6d8713373d973bbee000ea78b01524889da64
Christopher N. KATOYI <me@christopher2k.dev>
2022-10-13 17:26:52 -0400

    Chore: Optimizing authentication check flow CHR-63 (#3)

--- api/src/models/user_session.rs
+++ api/src/models/user_session.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use time::ext::NumericalDuration;
 use uuid::Uuid;
 
 use super::user::User;
@@ -9,15 +10,23 @@ pub struct UserSession {
     pub username: String,
     pub access_token: String,
     pub refresh_token: String,
+    pub expire_at: i64,
 }
 
 impl UserSession {
-    pub fn new(db_user: &User, access_token: &str, refresh_token: &str) -> Self {
+    pub fn new(db_user: &User, access_token: &str, refresh_token: &str, expire_in: i64) -> Self {
+        let now = time::OffsetDateTime::now_utc();
+        let expire_at = {
+            let future_date = now + expire_in.seconds();
+            future_date.unix_timestamp()
+        };
+
         Self {
             id: db_user.id.clone(),
             username: db_user.username.clone(),
             access_token: String::from(access_token),
             refresh_token: String::from(refresh_token),
+            expire_at,
         }
     }
 }
